e6ccc31b0c2305007f43221355a775a636dc1910
Sara Silva <saracsas2@gmail.com>
2020-09-21 21:32:03 -0300

    Usando for para as moedas
    Co-authored-by: Thamires <thamiresdepontesdasilva@gmail.com>

--- workshop/Assets/Part IV/Scripts/Moedas.cs
+++ workshop/Assets/Part IV/Scripts/Moedas.cs
@@ -7,11 +7,21 @@ public class Moedas : MonoBehaviour
 {
 
     public int coins = 0;
+    public int i = 0;
+
     public Text CoinText;
+    public List<GameObject> Coins;  
+
+
+    //public List<GameObject> Coins = {c1, c2, c3}; 
 
     public GameObject Coin;
     public GameObject Coin1;
     public GameObject Coin2;
+
+
+
+    
     
 
 
@@ -19,6 +29,14 @@ public class Moedas : MonoBehaviour
     void Start()
     {
         CoinText.text = "Coins: " + coins;
+        Coins = new List<GameObject>();
+        
+        
+        Coins.Add(Coin) ;
+        Coins.Add(Coin1);
+        Coins.Add(Coin2);
+
+
     }
 
     void Awake() {
@@ -32,27 +50,46 @@ public class Moedas : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.CompareTag("Moeda")){
+        //GameObject[] Coins2 = new GameObject[] {Coin, Coin1, Coin2};
+        //Coins.AddRange(Coins2);
 
-            coins++;
-            CoinText.text = "Coins: " + coins;
-            Coin.transform.position = new Vector3(Coin.transform.position.x, 10, -100);
-                      
-        }
+      
+        foreach (GameObject c in Coins){
+           // Debug.Log("Quantidade Coins: " + Coins.Count());
+          //  Debug.Log("IIII: " + i);
+           // Debug.Log("COOINS: " + coins);
 
-        if(other.gameObject.CompareTag("Moeda1")){
-            coins++;
-            CoinText.text = "Coins: " + coins;
-            Coin1.transform.position = new Vector3(Coin1.transform.position.x, 10, -100);
-                      
-        }
 
-        if(other.gameObject.CompareTag("Moeda2")){
-            coins++;
-            CoinText.text = "Coins: " + coins;
-            Coin2.transform.position = new Vector3(Coin2.transform.position.x, 10, -100);
-                      
+            if(other.gameObject.CompareTag("Moeda" + i)){
+                Debug.Log("MOOOOEEDDA: " + other.gameObject.CompareTag("Moeda" + i));
+
+                i++;
+                coins++;
+                CoinText.text = "Coins: " + coins;
+                c.transform.position = new Vector3(c.transform.position.x, 10, -100);
+            }
         }
+        // if(other.gameObject.CompareTag("Moeda")){
+
+        //     coins++;
+        //     CoinText.text = "Coins: " + coins;
+        //     Coin.transform.position = new Vector3(Coin.transform.position.x, 10, -100);
+                      
+        // }
+
+        // if(other.gameObject.CompareTag("Moeda1")){
+        //     coins++;
+        //     CoinText.text = "Coins: " + coins;
+        //     Coin1.transform.position = new Vector3(Coin1.transform.position.x, 10, -100);
+                      
+        // }
+
+        // if(other.gameObject.CompareTag("Moeda2")){
+        //     coins++;
+        //     CoinText.text = "Coins: " + coins;
+        //     Coin2.transform.position = new Vector3(Coin2.transform.position.x, 10, -100);
+                      
+        // }
         
 
     }
